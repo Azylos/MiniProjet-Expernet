71d943a0fa30cebb317b823fcf12bb41718f972f
Toma BARRET <tomabarret@outlook.fr>
2024-10-25 15:26:47 +0400

    accueil début style

--- database/requete.php
+++ database/requete.php
@@ -15,6 +15,27 @@
         return $result->fetch(PDO::FETCH_ASSOC);
     }
 
+    function displayGame() {
+        global $connexion;
+        $req = "SELECT JeuxID, Titre, Image, Genre, Tarif FROM vue_jeux_details";
+        $result = $connexion->query($req);
+        return $result;
+    }
+
+    function InWishlist($idUtilisateur, $idJeux) {
+        global $connexion;
+        $req = "SELECT COUNT(*) AS count FROM vouloir WHERE IdUtilisateurs = $idUtilisateur AND IdJeux = $idJeux";
+        $result = $connexion->query($req);
+        
+        if ($result && $result->rowCount() > 0) {
+            $row = $result->fetch(PDO::FETCH_ASSOC);
+            $count = intval($row['count']);
+            return $count > 0; // Renvoie vrai si une correspondance est trouvée, sinon faux
+        } else {
+            return false; // Renvoie faux si une erreur s'est produite ou si aucune correspondance n'a été trouvée
+        }
+    }
+
     function AddGame($idEditeur, $idGenre, $titre, $description, $dateDeSortie, $image) {
         global $connexion;
         $req = "INSERT INTO jeux (IdEditeur, IdGenre, titre, description, dateDeSortie, image) VALUES (?, ?, ?, ?, ?, ?)";
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
   session_start(); 
-//   require_once("./database/requeteDB.php");
+  require_once('./database/requete.php');
 
   if(isset($_SESSION['admin'])) {
     $id = $_SESSION['admin']['id'];
@@ -24,6 +24,8 @@
         }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOM3cxh5TWAoaDyR37ZjxZ6w5e7q2Jvp4k0bslQ" crossorigin="anonymous">
+
 </head>
 <body>
     <?php require_once('includes/navbar.php') ?>
@@ -37,19 +39,19 @@
         </div>
         <!-- Item 2 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./asset/images/categories-01.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="https://lh6.googleusercontent.com/proxy/k2Cqd4aWed7GQgrAcKXKNYKFjODU6EkmaWAMR2hn4b7DYi6WjgZMy1XgMbHFmBecr2uG73YIO21bmxVf48dsdCIHMVlf7IJO9KKQdVTmwjNMvsmcEY_ZDNVPmJfCBkGkt9RXchsr9N7hawzo25JiJXiJu066-e4QGsnpLZUjUW_cccUzElOU0WBnGSbNpfBEOA" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 3 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./asset/images/categories-03.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="https://i.pinimg.com/originals/e4/29/41/e429410579ad13007bf15edabb3d44c9.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 4 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./asset/images/categories-04.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBPSmS51mU6kianEAvRsead5XvlE5ioYQ71A&s" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 5 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="https://www.fond-ecran-hd.net/Public/uploads/2020-07-10/thumbs-1/wallpapertitanfall2videogames.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
     </div>
     <!-- Slider indicators -->
@@ -79,6 +81,53 @@
     </button>
 </div>
 
+<div class="section trending py-10 bg-gray-100">
+  <div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <?php
+        $jeux = displayGame();
+        if ($jeux->rowCount() > 0) {
+          // Afficher les jeux avec les détails de l'éditeur, du genre et du prix
+          while($row = $jeux->fetch(PDO::FETCH_ASSOC)) {
+            if(isset($id)){
+              $InWishlist = InWishlist($id,$row['JeuxID']);
+            }
+              echo '<div class="trending-items bg-white shadow-md rounded-lg overflow-hidden transition-all transform hover:-translate-y-1 hover:shadow-lg">';
+              echo '<div class="thumb">';
+              echo '<a href="product-details.php?id='.$row['JeuxID'].'">';
+              echo '<img class="w-full h-48 object-cover" src="asset/images/'.$row['Image'].'" alt="">';
+              echo '</a>';
+              echo '<span class="price absolute top-2 right-2 bg-green-500 text-white px-2 py-1 rounded-full text-xs font-bold">'.$row['Tarif'].'€</span>';
+              echo '</div>';
+              echo '<div class="p-4">';
+              echo '<span class="category text-sm text-gray-500">'.$row['Genre'].'</span>';
+              echo '<h4 class="text-lg font-semibold mt-2 mb-4">'.$row['Titre'].'</h4>';
+              if(isset($id)){
+                if($InWishlist){
+                  echo '<a href="#" class="remove-wishlist text-red-500" data-jeuxid="' . $row['JeuxID'] . '">';
+                  echo '<i class="fa-solid fa-heart"></i>';
+                  echo '</a>';
+                } else {
+                  echo '<a href="#" class="add-wishlist text-gray-600 hover:text-green-500" data-jeuxid="' . $row['JeuxID'] . '">';
+                  echo '<i class="fa fa-shopping-bag"></i>';
+                  echo '</a>';
+                }
+              } else {
+                echo '<a href="profil/logIn.php" class="text-gray-600 hover:text-green-500">';
+                echo '<i class="fa fa-shopping-bag"></i>';
+                echo '</a>';
+              }
+              echo '</div>';
+              echo '</div>';
+          }
+          } else {
+              echo "0 résultats";
+          }
+          $connexion = null;
+        ?>
+    </div>
+  </div>
+</div>
 
 <?php require_once('./includes/footer.php') ?>    
     <script src="https://cdn.tailwindcss.com"></script>
@@ -95,5 +144,6 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    <script src="https://kit.fontawesome.com/2ac531f13a.js" crossorigin="anonymous"></script>
 </body>
 </html>
