24b51759d407c24abb5143d86579e83403c91c67
Toma BARRET <tomabarret@outlook.fr>
2024-10-25 11:00:24 +0400

    ajout de requete CRUD

--- database/requete.php
+++ database/requete.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once "./database/connexion.php";
+    require_once('T:/Expernet/miniProjet/25.10.2024/database/connexion.php');
 
     function ShowGames() {
         global $connexion;
@@ -8,4 +8,42 @@
         return $result;
     }
 
+    function AddGame($idEditeur, $idGenre, $titre, $description, $dateDeSortie, $image) {
+        global $connexion;
+        $req = "INSERT INTO jeux (IdEditeur, IdGenre, titre, description, dateDeSortie, image) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $connexion->prepare($req);
+        $stmt->bindParam(1, $idEditeur, PDO::PARAM_INT);
+        $stmt->bindParam(2, $idGenre, PDO::PARAM_INT);
+        $stmt->bindParam(3, $titre, PDO::PARAM_STR);
+        $stmt->bindParam(4, $description, PDO::PARAM_STR);
+        $stmt->bindParam(5, $dateDeSortie, PDO::PARAM_STR);
+        $stmt->bindParam(6, $image, PDO::PARAM_STR);
+        $result = $stmt->execute();
+        if ($result) {
+            // Récupérer l'ID du dernier jeu inséré
+            $lastInsertedId = $connexion->lastInsertId();
+            return $lastInsertedId;
+        } else {
+            return false;
+        }
+    }
     
+    function UpdateGame($idJeu, $titre, $description, $dateDeSortie, $idEditeur, $idGenre, $tarifJeux) {
+        global $connexion;
+        $stmt = $connexion->prepare("CALL ModifierJeu(?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bindParam(1, $idJeu, PDO::PARAM_INT);
+        $stmt->bindParam(2, $titre, PDO::PARAM_STR);
+        $stmt->bindParam(3, $description, PDO::PARAM_STR);
+        $stmt->bindParam(4, $dateDeSortie, PDO::PARAM_STR);
+        $stmt->bindParam(5, $idEditeur, PDO::PARAM_INT);
+        $stmt->bindParam(6, $idGenre, PDO::PARAM_INT);
+        $stmt->bindParam(7, $tarifJeux, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    
+    function DeleteGame($id) {
+        global $connexion;
+        $req = "DELETE FROM jeux WHERE id = $id";
+        $result = $connexion->exec($req);
+        return $result;
+    }
